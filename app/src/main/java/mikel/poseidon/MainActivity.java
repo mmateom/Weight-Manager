0111820043a97a39cd26cf1612f0f80688f8e2e4
mmateom <mikel@mbpdemimesonero.uni.mdx.ac.uk>
2016-11-14 16:03:01 +0000

    Initial commit

---
+++ app/src/main/java/mikel/poseidon/MainActivity.java
@@ -0,0 +1,28 @@
+package mikel.poseidon;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        }
+
+
+    }
+
