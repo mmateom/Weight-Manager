40b5aef40cbbf766129afddda7f3df53d3d14a4d
mmateom <mikel@mbpdemimesonero.uni.mdx.ac.uk>
2016-11-14 18:59:55 +0000

    Get weight intent

--- app/src/main/java/mikel/poseidon/MainActivity.java
+++ app/src/main/java/mikel/poseidon/MainActivity.java
@@ -1,10 +1,12 @@
 package mikel.poseidon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-
         setSupportActionBar(myToolbar);
 
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
         }
+    public void openGetWeight (View view){
 
+        Intent get_weight_intent = new Intent(this, GetWeight.class);
+        startActivity(get_weight_intent);
 
     }
+
+}
+
+
 
